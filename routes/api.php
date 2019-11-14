b7dd94c7556a38c9621cc1bb160a351ccbb0a2bd
rizky <rizkynear1@gmail.com>
2019-11-14 10:27:23 +0800

    fix api

--- routes/api.php
+++ routes/api.php
@@ -15,39 +15,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix((function() {
+    $locale = request()->segment(3); // use third segment as locale
+    
+    return LaravelLocalization::setLocale($locale);
+})())->group(function() {
+    Route::prefix('blogs')->group(function () {
+        Route::get('/', 'BlogController@showAll');
+        Route::get('{blog}', 'BlogController@showOne');
+    });
+
+    Route::prefix('second-sections')->group(function() { 
+        Route::get('/', 'SecondSectionController@showAll');
+        Route::get('{secondSection}', 'SecondSectionController@showOne');
+    });
+
+    Route::prefix('testimonials')->group(function() {
+        Route::get('/', 'TestimonialController@showAll');
+        Route::get('{testimonial}', 'TestimonialController@showOne');
+    });
 });
+
 
 Route::group(['prefix' => 'destinations'], function() {
     Route::get('/', 'DestinationController@showAll');
     Route::get('{destination}', 'DestinationController@showOne');
 });
 
-// Route::group(['prefix' => LaravelLocalization::setLocale()], function() {
-//     // Route::group(['prefix' => 'blogs'], function() {
-//         Route::get('blogs/', 'BlogController@showAll');
-//         Route::get('blogs/{blog}', 'BlogController@showOne');
-//     // });
-// });
+Route::group(['prefix' => 'sliders'], function() {
+    Route::get('/', 'SliderController@showAll');
+    Route::get('{slider}', 'SliderController@showOne');
+});
 
-// Route::prefix((function() {
-//     $locale = request()->segment(3);
-//     return LaravelLocalization::setLocale($locale);
-// }))->group((function() {
-//     Route::get('blogs/', 'BlogController@showAll');
-//     Route::get('blogs/{blog}', 'BlogController@showOne');
-// }));
-
-// Route::prefix('v1')->group(function() {
-    
-Route::prefix((function() {
-    $locale = request()->segment(3); // use third segment as locale
-    
-    return LaravelLocalization::setLocale($locale);
-})())->group(function() {
-    Route::get('blogs/', 'BlogController@showAll');
-    Route::get('blogs/{blog}', 'BlogController@showOne');
+Route::group(['prefix' => 'boats'], function() {
+    Route::get('/', 'BoatController@showAll');
+    Route::get('{boat}', 'BoatController@showOne');
 });
 
 // Route::get('create', function() {
@@ -66,23 +72,3 @@ Route::prefix((function() {
 //         'description_id' => $blog->translate('id')->description,
 //     ]);
 // });
-
-Route::group(['prefix' => 'testimonials'], function() {
-    Route::get('/', 'TestimonialController@showAll');
-    Route::get('{testimonial}', 'TestimonialController@showOne');
-});
-
-Route::group(['prefix' => 'second-sections'], function() {
-    Route::get('/', 'SecondSectionController@showAll');
-    Route::get('{secondSection}', 'SecondSectionController@showOne');
-});
-
-Route::group(['prefix' => 'sliders'], function() {
-    Route::get('/', 'SliderController@showAll');
-    Route::get('{slider}', 'SliderController@showOne');
-});
-
-Route::group(['prefix' => 'boats'], function() {
-    Route::get('/', 'BoatController@showAll');
-    Route::get('{boat}', 'BoatController@showOne');
-});
