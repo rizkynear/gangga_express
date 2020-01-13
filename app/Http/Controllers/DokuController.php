c5149f7e96e6caba945ce60aeb0089f62c2323c0
rizky <rizkynear1@gmail.com>
2020-01-13 13:11:17 +0800

    test doku

--- app/Http/Controllers/DokuController.php
+++ app/Http/Controllers/DokuController.php
@@ -19,7 +19,7 @@ class DokuController extends Controller
 
         $checkWords = Doku::checkWords($request);
 
-        if ($checkWords === true && (int)$request->RESPONSECODE === 0000 && $request->RESULTMSG === 'SUCCESS' && $booking->paid_status !== 1 && $request->VERIFYSTATUS === 'APPROVE') {
+        if ($checkWords === true && (int)$request->RESPONSECODE === 0000 && $request->RESULTMSG === 'SUCCESS' && $booking->paid_status !== 1) {
             $booking->update(['paid_status' => 1]);
 
             return 'CONTINUE';
