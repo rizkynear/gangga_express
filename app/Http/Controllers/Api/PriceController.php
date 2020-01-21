58a0b653cd664437089570e1353cd824caae3cb0
rizky <rizkynear1@gmail.com>
2020-01-21 16:22:28 +0800

    cors fix

--- app/Http/Controllers/Api/PriceController.php
+++ app/Http/Controllers/Api/PriceController.php
@@ -64,8 +64,6 @@ class PriceController extends Controller
                     'totalPrice' => $total['totalPrice']
                 ]
             ] 
-        ])->header('Access-Control-Allow-Origin', '*')
-          ->header('Access-Control-Allow-Methods', '*')
-          ->header('Access-Control-Allow-Headers', '*');
+        ]);
     }
 }
